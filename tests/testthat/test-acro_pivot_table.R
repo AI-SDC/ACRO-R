8ab92ab4b4a6dbcb0c5df69585530c209f6471d6
Richard Preen <rpreen@gmail.com>
2024-08-15 11:31:26 +0100

    fix pivot_table test

--- tests/testthat/test-acro_pivot_table.R
+++ tests/testthat/test-acro_pivot_table.R
@@ -6,8 +6,8 @@ test_that("acro_pivot_table without initialising ACRO object first", {
 test_that("acro_pivot_table works", {
   testthat::skip_on_cran()
   expected_table <- data.frame(
-    "mean   children" = c(3.272222, 3.242593, 3.241667),
-    "std   children" = c(2.48458394, 2.43848908, 2.42963966),
+    "('mean', 'children')" = c(3.272222, 3.242593, 3.241667),
+    "('std', 'children')" = c(2.48458394, 2.43848908, 2.42963966),
     check.names = FALSE
   )
 
