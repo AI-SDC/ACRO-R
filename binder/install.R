f5fadb141ab7da46d7dca2a788050eb175ae2772
Richard Preen <rpreen@gmail.com>
2025-09-17 16:29:08 +0100

    move to binder folder

--- binder/install.R
+++ binder/install.R
@@ -1,6 +1,8 @@
 # Install from CRAN
+install.packages("reticulate")
+install.packages("farff")
+install.packages("IRkernel")
 install.packages("acro")
 
 # Make R kernel available in Jupyter
-install.packages("IRkernel")
 IRkernel::installspec(user = FALSE)
