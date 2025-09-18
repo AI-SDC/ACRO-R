948d3f5cbaed7a636054c1507aad84ad8f9882a1
Richard Preen <rpreen@gmail.com>
2025-09-18 16:29:39 +0100

    update

--- binder/install.R
+++ binder/install.R
@@ -1,8 +1,3 @@
-# Install from CRAN
 install.packages("reticulate")
 install.packages("farff")
-install.packages("IRkernel")
 install.packages("acro")
-
-# Make R kernel available in Jupyter
-IRkernel::installspec(user = FALSE)
