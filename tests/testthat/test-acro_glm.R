34e7a0a23cfff237ea0c4bf3cd8d8c389ccef872
Jessica Ikechukwu <Jessica.Ikechukwu@uwe.ac.uk>
2025-11-27 10:46:51 +0000

    Update test-acro_glm.R
    Signed-off-by: Jessica Ikechukwu <Jessica.Ikechukwu@uwe.ac.uk>

--- tests/testthat/test-acro_glm.R
+++ tests/testthat/test-acro_glm.R
@@ -5,6 +5,7 @@ nursery_data$recommend[which(nursery_data$recommend == "very_recom")] <- "2"
 nursery_data$recommend[which(nursery_data$recommend == "priority")] <- "3"
 nursery_data$recommend[which(nursery_data$recommend == "spec_prior")] <- "4"
 nursery_data$recommend <- as.numeric(nursery_data$recommend)
+
 # extract relevant columns
 df <- nursery_data[, c("finance", "children")]
 # drop rows with missing values
@@ -15,22 +16,26 @@ df <- transform(df, finance = as.numeric(finance))
 df$finance <- df$finance - 1
 # formula to fit
 formula <- "finance ~ children"
+
 test_that("acro_glm without initialising ACRO object first", {
   acroEnv$ac <- NULL
   expect_error(acro_glm(formula = formula, data = df, family = "probit"), "ACRO has not been initialised. Please first call acro_init()")
 })
+
 test_that("acro_glm with probit as a family works", {
   testthat::skip_on_cran()
   acro_init()
   model <- acro_glm(formula = formula, data = df, family = "probit")
   expect_s3_class(model, "statsmodels.iolib.summary.Summary")
 })
+
 test_that("acro_glm with logit as a family works", {
   testthat::skip_on_cran()
   acro_init()
   model <- acro_glm(formula = formula, data = df, family = "logit")
   expect_s3_class(model, "statsmodels.iolib.summary.Summary")
 })
+
 test_that("acro_glm through an error if the family is not recognised", {
   testthat::skip_on_cran()
   acro_init()
