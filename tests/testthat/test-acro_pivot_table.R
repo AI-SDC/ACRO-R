2ab4ba096f46514b0bf4eeafa01e773db83b02d2
Jim-smith <jim-smith@users.noreply.github.com>
2026-02-02 12:46:17 +0000

    cleaning code

--- tests/testthat/test-acro_pivot_table.R
+++ tests/testthat/test-acro_pivot_table.R
@@ -6,10 +6,8 @@ test_that("acro_pivot_table without initialising ACRO object first", {
 test_that("acro_pivot_table works", {
   testthat::skip_on_cran()
   expected_table <- data.frame(
-#    "(mean', 'children')" = c(3.272222, 3.242593, 3.241667),
-#    "('std', 'children')" = c(2.48458394, 2.43848908, 2.42963966),
-    "mean children" = c(3.272222, 3.242593, 3.241667),
-    "std   children" = c(2.48458394, 2.43848908, 2.42963966),
+    "('mean', 'children')" = c(3.272222, 3.242593, 3.241667),
+    "('std', 'children')" = c(2.48458394, 2.43848908, 2.42963966),
     check.names = FALSE
   )
 
@@ -19,7 +17,5 @@ test_that("acro_pivot_table works", {
 
   acro_init()
   table <- acro_pivot_table(data = nursery_data, index = "parents", values = "children", aggfunc = list("mean", "std"))
-  print( table)
-  print(expected_table)
   expect_equal(table[, -1, drop = FALSE], expected_table[, -1, drop = FALSE])
 })
