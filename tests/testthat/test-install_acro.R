7810f95d425e77695c8d18f3a855338c19bd28cd
mahaalbashir <mahaalialbashir@gmail.com>
2026-06-25 16:03:05 +0100

    fixing the CI failing for conda tests

--- tests/testthat/test-install_acro.R
+++ tests/testthat/test-install_acro.R
@@ -1,21 +1,9 @@
-test_that("acro is installed in the python virtual environment", {
+test_that("acro is installed in the virtual environment", {
   testthat::skip_on_cran()
   test_envname <- "test-acro-env"
 
   # Run the acro_init function
   acro_init(envname = test_envname)
-
-  # Check if 'acro' is installed in the correct environment
-  is_installed <- reticulate::py_module_available("acro")
-  expect_true(is_installed)
-})
-
-test_that("acro is installed in the conda environment", {
-  testthat::skip_on_cran()
-  test_envname <- "test-acro-env"
-
-  # Run the acro_init function
-  acro_init(envname = test_envname, use_conda = TRUE)
 
   # Check if 'acro' is installed in the correct environment
   is_installed <- reticulate::py_module_available("acro")
