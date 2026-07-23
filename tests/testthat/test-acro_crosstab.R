73e67ca81826b775a02c8a7295fa4f2483e925f4
mahaalbashir <mahaalialbashir@gmail.com>
2026-07-23 14:14:25 +0100

    catching calls to unsupported aggregation function in R

--- tests/testthat/test-acro_crosstab.R
+++ tests/testthat/test-acro_crosstab.R
@@ -28,3 +28,32 @@ test_that("acro_crosstab works with margins", {
 
   expect_equal(unname(as.matrix(r_table)), unname(as.matrix(p_table)))
 })
+
+test_that("acro_crosstab works with aggregation function", {
+  # Creating the data frame with the specified values and row names
+  testthat::skip_on_cran()
+  expected_table <- data.frame(
+    convenient = c(3.25, 3.23, 3.25),
+    inconv = c(3.26, 3.26, 3.24)
+  )
+
+  # Assigning row names to the data frame
+  row.names(expected_table) <- c("not_recom", "priority", "recommended")
+
+  acro_init()
+  table <- acro_crosstab(index = nursery_data[, c("health")], columns = nursery_data[, c("finance")], values = nursery_data[, c("children")], aggfunc = "mean")
+  expect_equal(table[, -1, drop = FALSE], expected_table[, -1, drop = FALSE], tolerance = 0.01)
+})
+
+test_that("acro_crosstab throws an error for unsupported aggregation functions", {
+  acro_init()
+  expect_error(
+    acro_crosstab(
+      index = nursery_data$health,
+      columns = nursery_data$finance,
+      values = nursery_data$children,
+      aggfunc = "max"
+    ),
+    "Unsupported aggregation function provided"
+  )
+})
